650789e4c9c17659bd8911988b5cdcc56ce8faad
frameff2551 <166424685@srithatpit.ac.th>
2025-01-17 11:28:58 +0700

    Add files via upload

---
+++ h1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>เบื้องต้น</title>
+</head>
+<body>
+<p>-----การแสดงข้อความ-----</p>
+        <div>
+            <h1>HTMLเบื้องต้น(h1) </h1>
+</div>
+<div>
+             <h2>HTMLเบื้องต้น(h2) </h2>
+             <h3>HTMLเบื้องต้น(h2) </h3>
+             <h4>HTMLเบื้องต้น(h2) </h4>
+             <h5>HTMLเบื้องต้น(h2) </h5>
+             <h6>HTMLเบื้องต้น(h2) </h6>
+</div>
+    <p>-----การแสดงข้อความ-----</p>    
+    <div>
+        <d>ปนัดดา มุงเคน</b><br>
+        <i>ปนัดดา มุงเคน</i><br>
+        <u>ปนัดดา มุงเคน</u><br>
+</div>
+<p><b>-----การ Link-----</b></p>
+<div>
+   <!-- Link หน้าเดี่ยว -->
+   <a href ="https://www.google.co.th">google</a><br>
+</body>
+</html>
